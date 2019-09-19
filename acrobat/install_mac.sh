e2f1ca784d12bebf2ead6ca2eefb42aed69f51e8
sherylynn <352281674@qq.com>
2019-09-19 21:18:20 +0800

    add script for install and uninstall

---
+++ acrobat/install_mac.sh
@@ -0,0 +1,3 @@
+sudo cp folder_level.js /Applications/Adobe\ Acrobat\ Reader\ DC.app/Contents/Resources/JavaScripts/
+cd /Applications/Adobe\ Acrobat\ Reader\ DC.app/Contents/Resources/JavaScripts
+pwd
---
+++ acrobat/uninstall_mac.sh
@@ -0,0 +1,3 @@
+cd /Applications/Adobe\ Acrobat\ Reader\ DC.app/Contents/Resources/JavaScripts
+
+sudo rm -rf folder_level.js
