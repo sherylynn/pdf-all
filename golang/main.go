36f6e039f11ce62388260499b21e51353d25efd4
sherylynn <352281674@qq.com>
2020-03-16 00:57:09 +0800

    change port to 6666

--- golang/main.go
+++ golang/main.go
@@ -101,7 +101,7 @@ func get_latest_progress(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/update_progress", update_progress)         //设置访问的路由
 	http.HandleFunc("/get_latest_progress", get_latest_progress) //设置访问的路由
-	err := http.ListenAndServe(":10000", nil)                    //设置监听的端口
+	err := http.ListenAndServe(":6666", nil)                    //设置监听的端口
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
